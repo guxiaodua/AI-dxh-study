62e74e72e891670d607552caa84cf09ac10b5dc1
guxiaodua <2855121069@qq.com>
2026-07-25 01:54:36 +0800

    openai历史消息

--- 20260723/02-openAi.py
+++ 20260723/02-openAi.py
@@ -7,17 +7,34 @@ client = OpenAI(
 response = client.chat.completions.create(
     model='qwen3.7-plus',
     messages=[
-        {"role":"system","content":"你是一个python专家，并且不会说废话简单回答"},
-        {"role":"assistant","content":"你好，我是python专家，人狠话不多，请问需要什么帮助吗？"},
-        {"role":"user","content":"输出1-99数字，使用python代码"},
+        {"role":"system","content":"你是一个AI助手，回答很简洁"},
+        {"role":"user","content":"小明有三个饼干"},
+        {"role":"assistant","content":"好的"},
+        {"role":"user","content":"小红有8个饼干"},
+        {"role":"assistant","content":"好的"},
+        {"role":"user","content":"总共有多少个饼干？"},
     ],
     stream=True, # 开启流式输出功能
 )
 # 3、处理结果
 # print(response.choices[0].message.content)
+# for chunk in response:
+#     print(
+#         chunk.choices[0].delta.content,
+#         end=" ", # 每一段之间以空格分隔
+#         flush=True # 立刻刷新缓冲区
+#     )
+
 for chunk in response:
-    print(
-        chunk.choices[0].delta.content,
-        end="&", # 每一段之间以&分隔
-        flush=True # 立刻刷新缓冲区
-    )
+    # 关键判断：空choices直接跳过
+    if not chunk.choices:
+        continue
+    delta = chunk.choices[0].delta
+    # content不为None才打印
+    if delta.content is not None:
+        print(
+            delta.content,
+            end=" ",
+            flush=True
+        )
+print("\n流式输出结束")
