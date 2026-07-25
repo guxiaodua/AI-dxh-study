03414d01b27e9871fb6ac100a5d5d9bfd6dcf070
guxiaodua <2855121069@qq.com>
2026-07-26 00:29:04 +0800

    langchain流式输出

--- 20260723/03-langchain.py
+++ 20260723/03-langchain.py
@@ -3,6 +3,7 @@ from langchain_community.llms.tongyi import Tongyi
 # 不要用qwen3.7-plus, qwen3.7-plus属于多模态模型，qwen-max是大语言模型
 model = Tongyi(model="qwen-max")
 # 调用invoke向模型提问
-res = model.invoke(input="你是谁，你可以做什么？")
+res = model.stream(input="你是谁，你可以做什么？")
 
-print(res)
+for chunk in res:
+    print(chunk,end="",flush=True)
--- 20260723/03-langchain本地访问ollama.py
+++ 20260723/03-langchain本地访问ollama.py
@@ -3,6 +3,7 @@ from langchain_ollama import OllamaLLM
 # 不要用qwen3.7-plus, qwen3.7-plus属于多模态模型，qwen-max是大语言模型
 model = OllamaLLM(model="qwen3.5:9b-q4_K_M")
 # 调用invoke向模型提问
-res = model.invoke(input="你是谁，你可以做什么？")
+res = model.stream(input="你是谁，你可以做什么？")
 
-print(res)
+for chunk in res:
+    print(chunk,end="",flush=True)
