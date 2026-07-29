caafa1733f43f64b16f76b9b275fbecdfbd5229b
guxiaodua <2855121069@qq.com>
2026-07-30 02:13:49 +0800

    langchain长期对话错误版本

--- 20260723/05-langchain长期会话记忆.py
+++ 20260723/05-langchain长期会话记忆.py
@@ -1,13 +1,21 @@
 import os,json
 from langchain_core.messages import message_to_dict, messages_from_dict
 from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_models.tongyi import ChatTongyi
+from typing import Sequence
+from langchain_core.messages import BaseMessage
+
 
 # message_to_dict: 单个消息对象（BaseMessage类实例） -> 字典
 # messages_from_dict: [字典、字典...] -> [消息、消息...]
 # AIMessage、HumanMessage、SystemMessage 都是BaseMessage的子类
 
 class FileChatMessageHistory(BaseChatMessageHistory):
-    def __int__(self, session_id, storage_path):
+    def __init__(self, session_id, storage_path):
         self.session_id = session_id        # 会话id
         self.storage_path = storage_path    # 不同会话id的存储文件，所在的文件夹路径
         # 完整的文件路径
@@ -43,3 +51,57 @@ class FileChatMessageHistory(BaseChatMessageHistory):
                     message_data = json.load(f)
             except FileNotFoundError:
                 return []
+
+        def clear(self) -> None:
+            with open(self.file_path, "w", encoding="utf-8") as f:
+                json.dump([], f)
+
+
+
+model = ChatTongyi(model="qwen3-max")
+# prompt = PromptTemplate.from_template(
+#     "你需要根据历史会话回答用户问题，对话历史：{chat_history}，用户提问：{input}，请回答"
+# )
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "你需要根据会话历史回答用户问题，对话历史："),
+        MessagesPlaceholder("chat_history"),
+        ("human", "请回答如下问题：{input}"),
+    ]
+)
+
+str_parser = StrOutputParser()
+
+def print_prompt(full_prompt):
+    print('='*10, full_prompt.to_string(), '='*10)
+    return full_prompt
+
+base_chain = prompt | print_prompt | model | str_parser
+
+# 相当于一个map
+def get_history(session_id):
+    return FileChatMessageHistory(session_id, "./chat_history")
+
+c_chain = RunnableWithMessageHistory(
+    base_chain,     # 被增强的原有chain
+    get_history,    # 通过会话id获取InMemoryChatMessageHistory类对象
+    input_messages_key="input",             # 表示用户输入在模板中的提问占位符
+    history_messages_key="chat_history",    # 表示用户输入在模板中的历史占位符
+)
+
+if __name__ == '__main__':
+    session_cfg = {
+        "configurable": {
+            "session_id": "user_001",
+        }
+    }
+
+    res = c_chain.invoke({"input": "小明有一只狗"}, session_cfg)
+    print("第一次执行: ", res)
+
+    res = c_chain.invoke({"input": "小红三个小鸡"}, session_cfg)
+    print("第二次执行: ", res)
+
+    # res = c_chain.invoke({"input": "一个共有多少个动物"}, session_cfg)
+    # print("第三次执行: ", res)
